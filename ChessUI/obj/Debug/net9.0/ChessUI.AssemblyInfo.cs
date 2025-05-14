599ba825a07cc29c2c38b1d8b6493f7b32250195
Vasyl Diakulych <vasyadiakulych@gmail.com>
2025-05-14 22:35:34 +0200

    Created ChessAI project

--- ChessUI/obj/Debug/net9.0/ChessUI.AssemblyInfo.cs
+++ ChessUI/obj/Debug/net9.0/ChessUI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ChessUI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e5d2ac4a18aca527f3ac997ef92bc7425c4aa06a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+53ff7381f8e77e05db08c4eafd9bc525c58880d5")]
 [assembly: System.Reflection.AssemblyProductAttribute("ChessUI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ChessUI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
